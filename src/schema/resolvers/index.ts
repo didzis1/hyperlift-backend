93c32c5151b62d5a766ef234a60c66b6d7d3c0a5
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-10-15 21:07:55 +0300

    Fixed a type error

--- src/schema/resolvers/index.ts
+++ src/schema/resolvers/index.ts
@@ -1,4 +1,4 @@
 import { UserResolver } from './UserResolver';
 import { RoutineResolver } from './RoutineResolver';
 
-export const resolvers = [UserResolver, RoutineResolver];
+export const resolvers = [UserResolver, RoutineResolver] as const;
