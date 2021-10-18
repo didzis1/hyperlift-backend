45a3202609aa1bd724645b8dab6391724a15a795
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-10-18 11:01:33 +0300

    Added a resolver that creates a routine

--- src/schema/resolvers/RoutineResolver.ts
+++ src/schema/resolvers/RoutineResolver.ts
@@ -1,7 +1,8 @@
 import 'reflect-metadata';
-import { Query, Resolver } from 'type-graphql';
+import { Arg, Mutation, Query, Resolver } from 'type-graphql';
 import { Routine } from '../../models/routine';
 import RoutineModel from '../../models/routine';
+import { NewRoutineInput } from './types/RoutineInput';
 
 @Resolver()
 export class RoutineResolver {
@@ -12,5 +13,27 @@ export class RoutineResolver {
     if (!getRoutines) throw new Error('Error while fetching routines');
 
     return getRoutines;
+  }
+
+  @Mutation(() => Routine)
+  async createRoutine(
+    @Arg('routineData') routineData: NewRoutineInput
+  ): Promise<Routine> {
+    const user = {
+      _id: '616d1bae70acbf1f2f748b6c'
+    };
+
+    const newRoutine = new RoutineModel({
+      ...routineData,
+      user: user._id
+    });
+
+    const savedRoutine = await newRoutine.save();
+
+    if (!savedRoutine)
+      throw new Error('Error while trying to save a routine...');
+
+    // Populate the user field so that you can return user information
+    return savedRoutine.populate('user');
   }
 }
