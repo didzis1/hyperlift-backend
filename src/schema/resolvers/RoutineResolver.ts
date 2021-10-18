bbd780e31c81fc562f815acff82f198850f0c792
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-10-18 10:34:54 +0300

    Resolver for fetching all routines

--- src/schema/resolvers/RoutineResolver.ts
+++ src/schema/resolvers/RoutineResolver.ts
@@ -1,7 +1,6 @@
 import 'reflect-metadata';
-import { Arg, Mutation, Query, Resolver } from 'type-graphql';
-import { Routine, WorkoutInput } from '../types/Routine';
-import UserModel from '../../models/user';
+import { Query, Resolver } from 'type-graphql';
+import { Routine } from '../../models/routine';
 import RoutineModel from '../../models/routine';
 
 @Resolver()
@@ -13,46 +12,5 @@ export class RoutineResolver {
     if (!getRoutines) throw new Error('Error while fetching routines');
 
     return getRoutines;
-  }
-
-  @Mutation(() => Routine)
-  async createRoutine(
-    @Arg('title') title: string,
-    @Arg('description') description: string,
-    @Arg('workouts', () => [WorkoutInput]) workouts: WorkoutInput[],
-    @Arg('userId') userId: string
-  ): Promise<Routine> {
-    const findUser = await UserModel.findOne({
-      _id: userId
-    });
-
-    if (!findUser) throw new Error('User not found.');
-
-    // Add user ID as a reference to who created the routine
-    const newRoutine = new RoutineModel({
-      title,
-      description,
-      workouts,
-      user: userId
-    });
-
-    // Save the new routine to the database
-    const savedRoutine = await newRoutine.save();
-
-    if (!savedRoutine)
-      throw new Error('An error occured while trying to create a new routine');
-
-    // Save the routine's ID to the user's routine array
-    findUser.routines = findUser?.routines?.concat(savedRoutine._id);
-
-    try {
-      await findUser.save();
-    } catch (error) {
-      throw new Error(
-        `An error occured while saving routine to user: ${error}`
-      );
-    }
-
-    return newRoutine;
   }
 }
