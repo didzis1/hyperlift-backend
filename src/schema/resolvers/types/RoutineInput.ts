84286b3f0d5cb5ec0851f5696083c29334bb136f
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-10-18 11:01:12 +0300

    Created input fields for routine, workout splits and exercises

--- src/schema/resolvers/types/RoutineInput.ts
+++ src/schema/resolvers/types/RoutineInput.ts
@@ -1,12 +1,35 @@
-import { ObjectId } from 'mongoose';
-import { InputType, Field, ID } from 'type-graphql';
-import { Routine, WorkoutSplit } from '../../../models/routine';
+import { InputType, Field, Int } from 'type-graphql';
+import { Exercise, Routine, WorkoutSplit } from '../../../models/routine';
 
 @InputType()
 export class NewRoutineInput implements Partial<Routine> {
-  @Field()
+  @Field({ nullable: true })
   description?: string;
 
-  @Field(() => [WorkoutSplit])
-  workouts: WorkoutSplit[];
+  @Field(() => [NewWorkoutSplitInput])
+  workouts: NewWorkoutSplitInput[];
+}
+
+@InputType()
+export class NewWorkoutSplitInput implements Partial<WorkoutSplit> {
+  @Field()
+  name: string;
+
+  @Field(() => [NewExerciseInput])
+  exercises: NewExerciseInput[];
+}
+
+@InputType()
+export class NewExerciseInput implements Partial<Exercise> {
+  @Field()
+  exerciseName: string;
+
+  @Field(() => Int)
+  reps: number;
+
+  @Field(() => Int)
+  sets: number;
+
+  @Field(() => Int, { nullable: true })
+  weight?: number;
 }
