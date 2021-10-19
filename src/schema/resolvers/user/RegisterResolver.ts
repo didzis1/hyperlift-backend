e7e9c27ab7f477a5263003a197235877130b6807
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-10-19 12:55:20 +0300

    Created RegisterResolver, creates new user

---
+++ src/schema/resolvers/user/RegisterResolver.ts
@@ -0,0 +1,29 @@
+import UserModel, { User } from '../../../models/user';
+import { Resolver, Mutation, Arg } from 'type-graphql';
+import { RegisterInput } from '../inputs/RegisterInput';
+import bcryptjs from 'bcryptjs';
+
+@Resolver()
+export class RegisterResolver {
+  @Mutation(() => User)
+  async register(
+    @Arg('registerInput')
+    { firstName, lastName, email, password }: RegisterInput
+  ): Promise<User> {
+    const salt = bcryptjs.genSaltSync(12);
+    const hashedPassword = bcryptjs.hashSync(password, salt);
+
+    const user = new UserModel({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword
+    });
+
+    const savedUser = await user.save();
+
+    if (!savedUser) throw new Error('User could not been saved...');
+
+    return savedUser;
+  }
+}
