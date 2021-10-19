9cdadb2967b7b7f39ba1868764f8f0e37809c721
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-10-19 18:46:54 +0300

    Check if user inputs an email

--- src/schema/resolvers/inputs/LoginInput.ts
+++ src/schema/resolvers/inputs/LoginInput.ts
@@ -1,10 +1,13 @@
 import 'reflect-metadata';
+import 'class-validator';
 import { User } from '../../../models/user';
 import { Field, InputType } from 'type-graphql';
+import { IsEmail } from 'class-validator';
 
 @InputType()
 export class LoginInput implements Partial<User> {
   @Field()
+  @IsEmail()
   email: string;
 
   @Field()
