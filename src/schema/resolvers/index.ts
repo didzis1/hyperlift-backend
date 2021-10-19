835f21e3045eef5684f45e4caa0a62627fe60e99
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-10-19 12:54:22 +0300

    Updated resolvers

--- src/schema/resolvers/index.ts
+++ src/schema/resolvers/index.ts
@@ -1,4 +1,11 @@
-import { UserResolver } from './UserResolver';
-import { RoutineResolver } from './RoutineResolver';
+import { RegisterResolver } from './user/RegisterResolver';
+import { LoginResolver } from './user/LoginResolver';
+import { RoutineResolver } from './routine/RoutineResolver';
+import { MeResolver } from './user/MeResolver';
 
-export const resolvers = [UserResolver, RoutineResolver] as const;
+export const resolvers = [
+  RegisterResolver,
+  RoutineResolver,
+  LoginResolver,
+  MeResolver
+] as const;
