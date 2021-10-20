a2c65d8df77fbbf8f7bdecfe2a250a413c2b76e3
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-10-20 13:43:14 +0300

    uppercased test

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,7 +4,7 @@ const PORT = process.env.PORT;
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 const MONGODB_URI =
-  process.env.NODE_ENV === 'test'
+  process.env.NODE_ENV === 'TEST'
     ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI;
 
