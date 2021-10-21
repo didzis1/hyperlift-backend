1713b804fd3eb2090046aa9ae602046072d47b8e
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-10-21 13:12:29 +0300

    Testing registration

--- src/__tests__/Register.test.ts
+++ src/__tests__/Register.test.ts
@@ -23,7 +23,7 @@ mutation Mutation($registerInput: RegisterInput!) {
 }`;
 
 describe('RegisterResolver', () => {
-  it('User is created in the database', async () => {
+  it('User can be created', async () => {
     const user = {
       firstName: faker.name.firstName(),
       lastName: faker.name.lastName(),
@@ -51,5 +51,54 @@ describe('RegisterResolver', () => {
     const userInDatabase = await UserModel.findOne({ email: user.email });
     expect(userInDatabase).toBeDefined(); // Expect if the user exists in the database
     expect(userInDatabase!.email).toBe(user.email); // Expect that the user has an email in the database
+  });
+
+  it('User with wrong email format is not created', async () => {
+    const user = {
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: 'not-an-email.com',
+      password: faker.internet.password()
+    };
+
+    const response = await graphQLCall({
+      source: registerMutation,
+      variableValues: {
+        registerInput: user
+      }
+    });
+
+    // user is not created
+    expect(response).toMatchObject({
+      data: null
+    });
+
+    // Error response also contains the constraints on what went wrong
+    expect(response!.errors![0].message).toBe('Argument Validation Error');
+  });
+
+  it('Email is already in use', async () => {
+    const user = {
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    };
+
+    await UserModel.create(user);
+
+    expect(await UserModel.findOne({ email: user.email })).toBeDefined(); // Check if user got saved to the database
+
+    const response = await graphQLCall({
+      source: registerMutation,
+      variableValues: {
+        registerInput: user
+      }
+    });
+
+    expect(response).toMatchObject({
+      data: null
+    });
+    expect(response!.errors![0].message).toBe('E-mail is already in use');
   });
 });
