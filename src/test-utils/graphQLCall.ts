f3dace87be93030e08b35ab2032e8755dfaae983
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-10-21 10:48:30 +0300

    Added context value for graphql call util function

--- src/test-utils/graphQLCall.ts
+++ src/test-utils/graphQLCall.ts
@@ -1,15 +1,21 @@
 import { graphql, GraphQLSchema } from 'graphql';
 import { Maybe } from 'type-graphql';
 import createSchema from '../schema';
+import { User } from '../models/user';
 
 type Options = {
   source: string;
   variableValues?: Maybe<{ [key: string]: any }>;
+  currentUser?: User | null;
 };
 
 let schema: GraphQLSchema;
 
-export const graphQLCall = async ({ source, variableValues }: Options) => {
+export const graphQLCall = async ({
+  source,
+  variableValues,
+  currentUser
+}: Options) => {
   // Create a schema if it doesn't exist
   // graphQLCall is called in tests multiple times but the schema is created once
   if (!schema) {
@@ -18,6 +24,9 @@ export const graphQLCall = async ({ source, variableValues }: Options) => {
   return graphql({
     schema,
     source,
-    variableValues
+    variableValues,
+    contextValue: {
+      currentUser
+    }
   });
 };
