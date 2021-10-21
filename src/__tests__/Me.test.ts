438406933da73ac7d90e813cb5ebd921daf2e319
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-10-21 13:12:35 +0300

    Test if user is logged in or not

---
+++ src/__tests__/Me.test.ts
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose';
+import { testConnection } from '../test-utils/testConnection';
+import faker from 'faker';
+import UserModel from '../models/user';
+import { graphQLCall } from '../test-utils/graphQLCall';
+
+beforeAll(async () => {
+  await testConnection();
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
+
+const meQuery = `
+{
+  me {
+    id
+    firstName
+    lastName
+    email
+  }
+}`;
+
+describe('Me', () => {
+  it('Get user from context', async () => {
+    const user = await UserModel.create({
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    });
+
+    // Normally user's ID is verified through token
+    // Here we assume that the user is logged in
+    const currentUser = await UserModel.findById(user._id);
+
+    const response = await graphQLCall({
+      source: meQuery,
+      currentUser
+    });
+
+    expect(response).toMatchObject({
+      data: {
+        me: {
+          id: `${user._id}`,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email
+        }
+      }
+    });
+  });
+});
