dfd07cacefa571c44f37857f5ff098c7844f1380
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-10-21 17:22:43 +0300

    Reworked config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,9 +3,12 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
-const MONGODB_URI =
-  process.env.NODE_ENV === 'TEST'
-    ? process.env.TEST_MONGODB_URI
-    : process.env.MONGODB_URI;
+let MONGODB_URI: string;
+
+if (process.env.NODE_ENV === 'TEST') {
+  MONGODB_URI = process.env.MONGODB_URI_TEST as string;
+} else if (process.env.NODE_ENV === 'DEVELOPMENT') {
+  MONGODB_URI = process.env.MONGODB_URI as string;
+}
 
 export { PORT, MONGODB_URI, JWT_SECRET_KEY };
