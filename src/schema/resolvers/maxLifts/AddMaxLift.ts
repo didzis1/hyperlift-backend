adefbf092e639e37f89784f85d254b971e003eb5
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-10-22 18:46:18 +0300

    Mutation resolver that creates new max lift for the user

---
+++ src/schema/resolvers/maxLifts/AddMaxLift.ts
@@ -0,0 +1,40 @@
+import { MyContext } from '../../../types/MyContext';
+import { Arg, Ctx, Mutation, Resolver } from 'type-graphql';
+import MaxLiftModel, { MaxLift } from '../../../models/maxLift';
+import UserModel from '../../../models/user';
+import { NewMaxLiftInput } from '../inputs/NewMaxLiftInput';
+
+@Resolver()
+export class AddMaxLiftResolver {
+  @Mutation(() => MaxLift)
+  async addMaxLift(
+    @Ctx() ctx: MyContext,
+    @Arg('maxLiftData') maxLiftData: NewMaxLiftInput
+  ): Promise<MaxLift> {
+    if (!ctx.currentUser) {
+      throw new Error('You must be authorized to add a new max lift');
+    }
+
+    const user = await UserModel.findById(ctx.currentUser.id);
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const newMaxLift = await MaxLiftModel.create(maxLiftData);
+
+    if (!newMaxLift) {
+      throw new Error('An error occured while trying to create a new max lift');
+    }
+
+    user.maxLifts.push(newMaxLift);
+
+    try {
+      await user.save();
+    } catch (error) {
+      throw new Error('An error occured while trying to save a new max lift');
+    }
+
+    return newMaxLift;
+  }
+}
