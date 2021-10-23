4f3a22a19492e64d9c6547aca5546c063c9d03aa
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-10-23 15:52:10 +0300

    MaxLift now is a simple object field not a model

--- src/models/maxLift.ts
+++ src/models/maxLift.ts
@@ -1,11 +1,10 @@
-import { ObjectId } from 'mongoose';
-import { ObjectType, Field, ID, Int } from 'type-graphql';
-import { getModelForClass, prop as Property } from '@typegoose/typegoose';
+import { ObjectType, Field, Int } from 'type-graphql';
+import { prop as Property } from '@typegoose/typegoose';
 
 @ObjectType()
 export class MaxLift {
-  @Field(() => ID)
-  id: ObjectId;
+  @Field(() => Int)
+  id: number;
 
   @Field()
   @Property({ required: true })
@@ -15,7 +14,3 @@ export class MaxLift {
   @Property({ required: true })
   weight: number;
 }
-
-const MaxLiftsModel = getModelForClass(MaxLift);
-
-export default MaxLiftsModel;
