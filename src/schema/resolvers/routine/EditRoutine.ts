4bdcbd0a567e930a4a5466c146adb38f1fcaeefb
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-12-12 11:48:58 +0200

    User can now update the routine

--- src/schema/resolvers/routine/EditRoutine.ts
+++ src/schema/resolvers/routine/EditRoutine.ts
@@ -11,13 +11,27 @@ export class EditRoutineResolver {
     @Arg('routineData') routineData: EditRoutineInput
   ): Promise<Routine> {
     if (!ctx.currentUser) {
-      throw new Error('You must be authorized to add a new max lift');
+      throw new Error('You must be authorized to add a new routine');
     }
 
     const oldRoutine = await RoutineModel.findById(routineData._id);
 
     if (!oldRoutine) throw new Error('Routine was not found');
 
-    return oldRoutine;
+    try {
+      await oldRoutine.update({
+        description: routineData.description,
+        workouts: routineData.workouts
+      });
+    } catch (error) {
+      throw new Error('An error occured while trying to edit the routine');
+    }
+
+    const updatedRoutine = await RoutineModel.findById(routineData._id);
+
+    if (!updatedRoutine)
+      throw new Error('Something went wrong while editing your routine');
+
+    return updatedRoutine;
   }
 }
